916211c8b957b18650c4016e8e0f93224a2ca245
Sabu <sabuw319@gmail.com>
2018-09-18 14:32:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(instrumentList, musicianList){
+ var beatleFacts = []
+  for(var i = 0; i > musicianList.length; i++){
+    beatleFacts.push(`${musicianList[i]} plays ${instrumentList[i]}`)
+  }
+  return beatleFacts
+}
+
+function johnLennonFacts(jlFacts){
+  while(i < jlFacts.length){
+    var i = 0
+    jlFacts[i] = jlFacts[i] + "!!!"
+    i++
+  }
+}
+
+function iLoveTheBeatles(n){
+  var crowdChant = []
+  do{
+    crowdChant.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+  return crowdChant
+}
+
